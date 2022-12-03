139e5a99dc5616a914a1478ea0ad4652fb896307
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-03 16:13:15 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+    let revStr = '';
+    for(let i=0; i<str.length; i++){
+        let temp = str.charAt(i);
+        revStr =   temp + revStr ;
+    }
+    return revStr;
 
 }
 module.exports = palindrome
+
+ 
