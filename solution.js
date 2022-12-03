03c0dca645b0707cd0bf9760adfc72d3f491813e
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-03 16:37:09 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,33 +1,15 @@
  
-// function palindrome(str){
-//     let revStr = '';
-//     for(let i=0; i<str.length; i++){
-//         let temp = str.charAt(i);
-//         revStr =   temp + revStr ;
-//     }
-//     if(revStr === str){
-// 		return(true)
-// 	}
-// 	else{
-// 		return(false)
-		
-// 	}
-
-// }
 function palindrome(str){
-	let lowerStr = str.toLowerCase()
-		 
-	
     let revStr = '';
-    for(let i=0; i<lowerStr.length; i++){
-        let temp = lowerStr.charAt(i);
+    for(let i=0; i<str.length; i++){
+        let temp = str.charAt(i);
         revStr =   temp + revStr ;
     }
-    if(revStr == lowerStr){
-		console.log(true)
+    if(revStr == str){
+		return(true)
 	}
 	else{
-		console.log(false)
+		return(false)
 		
 	}
 
